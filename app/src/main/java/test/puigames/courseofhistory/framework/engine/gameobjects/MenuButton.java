72d379ffb4c64e7d589e6e9d5efb942c29212c05
Christopher <cmorrow23@qub.ac.uk>
2017-03-13 16:22:57 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/app.iml
    #	app/src/main/java/test/puigames/courseofhistory/framework/engine/Level.java
    #	app/src/main/java/test/puigames/courseofhistory/framework/game/TestLevel.java
    #	build/intermediates/dex-cache/cache.xml
    
    uiElement created, MenuButton extends uiElement

--- app/src/main/java/test/puigames/courseofhistory/framework/engine/gameobjects/MenuButton.java
+++ app/src/main/java/test/puigames/courseofhistory/framework/engine/gameobjects/MenuButton.java
@@ -2,37 +2,92 @@ package test.puigames.courseofhistory.framework.engine.gameobjects;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 
 import test.puigames.courseofhistory.framework.engine.gameobjects.properties.BoundingBox;
 import test.puigames.courseofhistory.framework.engine.gameobjects.properties.Drawable;
 import test.puigames.courseofhistory.framework.engine.gameobjects.properties.Origin;
+import test.puigames.courseofhistory.framework.engine.inputfriends.InputBuddy;
+import test.puigames.courseofhistory.framework.engine.inputfriends.subfriends.Input;
 
 /**
 * Created by Christopher on 09/03/2017.
 */
 
-public class MenuButton extends GameObject implements Drawable {
+public class MenuButton extends uiElement implements Drawable{
 
-    Bitmap buttonImage;
+    protected Bitmap buttonImage;
     protected Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    public MenuButton(Bitmap buttonImage, Paint paint){
-        super(0, 0, 0, 0);
+    public MenuButton(Bitmap buttonImage, float spawnX, float spawnY, int width, int height){
+        super(spawnX, spawnY,  width,  height);
         this.buttonImage = buttonImage;
         this.boundingBox = new BoundingBox(width, height, origin);
     }
 
     @Override
     public void draw(Canvas canvas, float deltaTime) {
+
         canvas.drawBitmap(buttonImage, matrix, paint);
     }
 
-    public Bitmap getImage(){
+    public void update(InputBuddy inputBuddy, float deltaTime){
+        super.update(inputBuddy, deltaTime);
+
+//        for(Input.TouchEvent touchEvent : inputBuddy.getTouchEvents()) {
+////            if(this.boundingBox.isTouchOn(touchEvent)) {
+//////                changeScreen(screenName);
+////            }
+//        }
+    }
+
+    public Bitmap getButtonImage(){
         return buttonImage;
     }
 
-    public Origin getOrigin(){
+    public void setButtonImage(Bitmap buttonImage) {
+        this.buttonImage = buttonImage;
+    }
+
+    public Origin getOrigin() {
         return origin;
     }
+
+    public void setOrigin(Origin origin) {
+        this.origin = origin;
+    }
+
+    public Matrix getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(Matrix matrix) {
+        this.matrix = matrix;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public BoundingBox getBoundingBox() {
+        return boundingBox;
+    }
+
+    public void setBoundingBox(BoundingBox boundingBox) {
+        this.boundingBox = boundingBox;
+    }
+
 }
