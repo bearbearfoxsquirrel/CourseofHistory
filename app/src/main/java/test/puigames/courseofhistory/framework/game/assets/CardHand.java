ec4041595bf1060506810a3cda3f7ba7be3d4ef1
mjtod <mtodd12@qub.ac.uk>
2017-03-20 14:20:15 +0000

    Adding of card hand

---
+++ app/src/main/java/test/puigames/courseofhistory/framework/game/assets/CardHand.java
@@ -0,0 +1,65 @@
+package test.puigames.courseofhistory.framework.game.assets;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+import test.puigames.courseofhistory.framework.engine.gameobjects.Sprite;
+import test.puigames.courseofhistory.framework.engine.gameobjects.properties.Origin;
+import test.puigames.courseofhistory.framework.engine.inputfriends.InputBuddy;
+import test.puigames.courseofhistory.framework.engine.screen.scaling.Scaler;
+import test.puigames.courseofhistory.framework.game.CardArea;
+import test.puigames.courseofhistory.framework.game.assets.cards.CharacterCard;
+
+/**
+ * Created by mjtod on 13/03/2017.
+ */
+
+public class CardHand extends CardArea{
+    private ArrayList<CharacterCard> cardHand = new ArrayList<CharacterCard>();
+    private Origin[] handPositions;
+    Scaler scaler;
+
+
+    public CardHand(){
+        super(160, 40);
+
+
+        maxCardsInArea = 8;
+        handPositions = new Origin[maxCardsInArea];
+        for(int i = 1; i <= maxCardsInArea; i++)
+            handPositions[i - 1] = new Origin((width/maxCardsInArea) * i, this.origin.y + this.halfHeight/maxCardsInArea);
+
+
+
+    }
+
+    public void halfCardSize(CharacterCard characterCard){
+        characterCard.setHeight(characterCard.height*0.75f);
+        characterCard.setWidth(characterCard.width*0.75f);
+
+    }
+
+    public void addToHand(CharacterCard characterCard, ArrayList<Sprite> sprites) {
+        halfCardSize(characterCard);
+        super.addCardToArea(characterCard);
+        positionCardsInArea();
+        sprites.add(characterCard);
+    }
+
+    public void update(InputBuddy inputBuddy, float deltaTime) {
+        super.update(inputBuddy, deltaTime);
+        positionCardsInArea();
+    }
+
+    public void positionCardsInArea()
+    {
+        for (int i = 0; i < cardsInArea.size(); i++)
+        {
+//            if(!cardsInArea.get(i).origin.equals(positions[i]))
+            cardsInArea.get(i).setOrigin(new Origin(handPositions[i]));
+        }
+        //Log.d("Called", "positioned cards");
+    }
+
+}
