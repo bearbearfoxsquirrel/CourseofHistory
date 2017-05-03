38be777e820d359c149afb1332cf8f703adcc136
mjtod <mtodd12@qub.ac.uk>
2017-05-03 17:55:58 +0100

    Attempting new method of card stats using images and states, so that they may be updated by either abilities or attacking.
    Added new images as well as stat image class.
    
    Stat image now added to character card.
    
    Stats are now checked upon attack

---
+++ app/src/main/java/test/puigames/courseofhistory/framework/game/assets/StatImage.java
@@ -0,0 +1,109 @@
+package test.puigames.courseofhistory.framework.game.assets;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import test.puigames.courseofhistory.framework.engine.gameobjects.GameObject;
+import test.puigames.courseofhistory.framework.engine.gameobjects.Sprite;
+import test.puigames.courseofhistory.framework.engine.screen.Screen;
+
+/**
+ * Created by mjtod on 02/05/2017.
+ */
+
+
+public class StatImage extends Sprite {
+    private Number state = Number.UNASSIGNED;
+    private Bitmap[] numberImages = new Bitmap[10];
+
+    public StatImage(Screen screen, Bitmap[] numImages, int width, int height){
+        super(screen, numImages[0], width, height);
+
+        for(int i= 0; i < numImages.length; i++){
+            numberImages[i] = numImages[i];
+        }
+        UpdateStats();
+    }
+
+    @Override
+    public void setRotation(int rotation) {
+        super.setRotation(rotation);
+    }
+
+    public enum Number
+    {
+        ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, UNASSIGNED
+    }
+
+    @Override
+    public void place(Screen screen, float placementX, float placementY) {
+        super.place(screen, placementX, placementY);
+    }
+
+    public void UpdateStats(){
+        switch(state)
+        {
+            case ZERO:
+                bitmap = numberImages[0];
+                break;
+            case ONE:
+                bitmap = numberImages[1];
+                break;
+            case TWO:
+                bitmap = numberImages[2];
+                break;
+            case THREE:
+                bitmap = numberImages[3];
+                break;
+            case FOUR:
+                bitmap = numberImages[4];
+                break;
+            case FIVE:
+                bitmap = numberImages[5];
+                break;
+            case SIX:
+                bitmap = numberImages[6];
+                break;
+            case SEVEN:
+                bitmap = numberImages[7];
+                break;
+            case EIGHT:
+                bitmap = numberImages[8];
+                break;
+            case NINE:
+                bitmap = numberImages[9];
+                break;
+            default:
+                bitmap = numberImages[0];
+                break;
+
+        }
+    }
+
+
+
+    public Number getState() {
+        return state;
+    }
+
+    public void setState(Number state) {
+        this.state = state;
+    }
+
+    public Bitmap[] getNumberImages() {
+        return numberImages;
+    }
+
+    public void setNumberImages(Bitmap[] numberImages) {
+        this.numberImages = numberImages;
+    }
+
+    @Override
+    public void draw(Canvas canvas, float deltatime){
+        super.draw(canvas, deltatime);
+    }
+
+
+
+
+}
--- app/src/main/java/test/puigames/courseofhistory/framework/game/assets/cards/CharacterCard.java
+++ app/src/main/java/test/puigames/courseofhistory/framework/game/assets/cards/CharacterCard.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 
 import test.puigames.courseofhistory.framework.engine.screen.Screen;
+import test.puigames.courseofhistory.framework.game.assets.StatImage;
 import test.puigames.courseofhistory.framework.game.assets.players.events.Damageable;
 
 /**
@@ -29,9 +30,10 @@ public class CharacterCard extends Card implements Damageable.Attackable {
     private float attackLocationY = 60f;
     private float healthLocationX = 40f;
     private float healthLocationY = 60f;
+    private StatImage [] statImages = new StatImage[3];
 
 
-    public CharacterCard(Screen screen, Bitmap cardImage, String name, String description, int mana, int attack, int health, String abilityDescription) {
+    public CharacterCard(Screen screen, StatImage[] statImages, Bitmap cardImage, String name, String description, int mana, int attack, int health, String abilityDescription) {
         super(screen, cardImage);
         this.name = name;
         this.description = description;
@@ -42,23 +44,82 @@ public class CharacterCard extends Card implements Damageable.Attackable {
         this.currentAttackEnergy = 1;
         this.maxAttackEnergy = 1;
 
+        for(int i = 0; i < statImages.length; i++){
+            this.statImages[i] = statImages[i];
+        }
+
+        CheckStat(mana,this.statImages[0]);
+        CheckStat(health, this.statImages[1]);
+        CheckStat(attack, this.statImages[2]);
+        UpdateCardStats();
+        statImages[0].place(screen, manaLocationX, manaLocationY);
+        statImages[1].place(screen, attackLocationX, attackLocationY);
+        statImages[2].place(screen, healthLocationX, healthLocationY);
+
         paint.setColor(Color.rgb(255,255, 255));
         paint.setTextSize(12);
+    }
+
+    public void UpdateCardStats(){
+        for(int i = 0; i < statImages.length; i++){
+            statImages[i].UpdateStats();
+            statImages[i].setRotation(rotation);
+
+        }
+    }
+
+    public void CheckStat(int statToCheck, StatImage statImage){
+        switch(statToCheck) {
+            case 0:
+                statImage.setState(StatImage.Number.ZERO);
+                break;
+            case 1:
+                statImage.setState(StatImage.Number.ONE);
+                break;
+            case 2:
+                statImage.setState(StatImage.Number.TWO);
+                break;
+            case 3:
+                statImage.setState(StatImage.Number.THREE);
+                break;
+            case 4:
+                statImage.setState(StatImage.Number.FOUR);
+                break;
+            case 5:
+                statImage.setState(StatImage.Number.FIVE);
+                break;
+            case 6:
+                statImage.setState(StatImage.Number.SIX);
+                break;
+            case 7:
+                statImage.setState(StatImage.Number.SEVEN);
+                break;
+            case 8:
+                statImage.setState(StatImage.Number.EIGHT);
+                break;
+            case 9:
+                statImage.setState(StatImage.Number.NINE);
+                break;
+            default:
+                statImage.setState(StatImage.Number.UNASSIGNED);
+                break;
+        }
+
     }
 
     @Override
     public void draw(Canvas canvas, float deltatime){
         super.draw(canvas, deltatime);
-        drawCardStats(canvas);
+        //drawCardStats(canvas);
     }
 
 
-    public void drawCardStats(Canvas canvas) {
+    /*public void drawCardStats(Canvas canvas) {
         //To update it
         canvas.drawText(Integer.toString(mana), (getOrigin().getOriginX())+manaLocationX, (getOrigin().getOriginX())+manaLocationY, paint);
         canvas.drawText(Integer.toString(attack), (getOrigin().getOriginX())+attackLocationX, (getOrigin().getOriginY())+attackLocationY, paint);
         canvas.drawText(Integer.toString(health), (getOrigin().getOriginX())+healthLocationX, (getOrigin().getOriginY())+healthLocationY, paint);
-    }
+    }*/
 
     public void attack(Damageable recipientOfThyFatalBlow) {
         recipientOfThyFatalBlow.applyDamage(this.attack);
